0a6647257caf5ec2d0a0a62da4400d6d5d14072c
Sean H <sean.haugen560@gmail.com>
2022-08-05 06:36:00 -0500

    Anceint rome map upate

--- script.js
+++ script.js
@@ -1,58 +1,27 @@
+import {roll} from "./helper.js"
 
+let newRoll = roll()
 
-
-//----------------------------------------
-
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyBXUI1wqPf9jC08'}).base('app21ZWp3xLPn6TG4');
-
-const table = base('People');
-
-const getRecords = async () => {
-    const records = await table
-    .select()
-    .firstPage();
-    console.log(records)
+let toHit = dc => {
+    if (newRoll > dc) {
+        return "success"
+    } else {
+        return "fail"
+    }
 }
 
+console.log(toHit(10))
 
-const getRecordById = async (id) => {
-    const record = await table.find(id);
-    console.log(record)
-}
 
-// getRecordById('rec7nKXFDGJ3E97Ns')
 
-const createRecord = async (fields) => {
-    const createdRecord = await table.create(fields);
-    console.log(createdRecord);
-}
 
-const updatedRecord = async (id, fields) => {
-    const updatedRecord = await table.update(id, fields);
-    console.log(minifyRecord(updatedRecord))
-}
 
-const minifyRecord = (record) => {
-    return {
-        id: record.id,
-        fields: record.fields
-    }   
-}
+let button = document.querySelector('#button');
+let image = document.querySelector('#img')
 
-// updatedRecord("recp3MbYnvXwkwuHi" , {
-//     Name: "Updated"
-// })
-
-createRecord({
-    Name: "hobbit",
-    About: "burger",
+button.addEventListener('click', ()=> {
+    image.classList.toggle('reveal');
 })
-
-// getRecords();
-
-
-
 
 
 
